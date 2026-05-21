96f39a349404eb31ed8cd7f06607ac17fd2fa22e
jose.ccna <jose.ccna@gmail.com>
2026-05-20 22:04:54 -0300

    JWT

--- 02/ControleEstoque/ControleEstoque.API/Program.cs
+++ 02/ControleEstoque/ControleEstoque.API/Program.cs
@@ -1,6 +1,9 @@
 using ControleEstoque.API.Data;
 using ControleEstoque.API.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -10,17 +13,38 @@ builder.Services.AddDbContext<AppDbContext>(opt
     => opt.UseSqlServer(builder.Configuration.GetConnectionString("Default")));
 
 // Registro do Service usando o ciclo de vida Scoped (uma instancia por requisicao)
+// Configuração de injeção de dependência para os serviços da aplicação
 builder.Services.AddScoped<IPedidoService, PedidoService>();
 builder.Services.AddScoped<IProdutoService, ProdutoService>();
 builder.Services.AddScoped<IFornecedorService, FornecedorService>();
 builder.Services.AddScoped<IUsuarioService, UsuarioService>();
 builder.Services.AddScoped<IContaReceberService, ContaReceberService>();
 builder.Services.AddScoped<IPasswordService, PasswordService>();
+builder.Services.AddScoped<ITokenService, TokenService>();
 builder.Services.AddControllers().AddJsonOptions(options =>
 {
-    // encerra o erro de refer�ncia c�clica de objetos para o JSON
+    // encerra o erro de referência cíclica de objetos para o JSON
     options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
 });
+
+
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = builder.Configuration["Jwt:Emissor"],
+            ValidAudience = builder.Configuration["Jwt:Audiencia"],
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:ChaveSecreta"])) ??
+            throw new InvalidOperationException("Chave JWT não configurada")
+        };
+    });
+
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
